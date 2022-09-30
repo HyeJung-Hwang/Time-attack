7ab5ddd2b7fa9f3bf2761c0f9f3d8bcb8dbea7f7
HyeJung-Hwang <root1031@ewhain.net>
2022-09-30 16:47:28 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+from torch.utils.data import DataLoader
 from torch.utils.data import Dataset,DataLoader
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
@@ -7,6 +8,7 @@ import time
 import copy
 import torch
 from torch import Tensor
+from torch.utils.data import Dataset,DataLoader
 import glob
 import math
 import torch.nn as nn
@@ -16,36 +18,17 @@ import time
 import numpy as np
 import pandas as pd
 import os
-from models import LSTM_FC,MLP_BN
-from data import load_data,load_test_data
+import matplotlib.pyplot as plt
+from sklearn.metrics import auc, plot_precision_recall_curve, precision_recall_curve
+from models import CNN
+from data import load_test_data
 
-def BCELoss_class_weighted(weights):
 
-    def loss(input, target):
-        input = torch.clamp(input,min=1e-7,max=1-1e-7)
-        bce = - weights[1] * target * torch.log(input) - (1 - target) * weights[0] * torch.log(1 - input)
-        return torch.mean(bce)
-
-    return loss
-
-def evaluate(model, test_loader,DEVICE):
-    criterion = BCELoss_class_weighted(torch.Tensor([1, 20]))
-    model.eval()      
-    test_loss = 0     
-    correct = 0       
     
-    with torch.no_grad(): 
-        for data, target in test_loader:
-            data, target = data.to(DEVICE), target.to(DEVICE)     
-            output = model(data)                                  
-            test_loss += float(criterion(output.to(torch.float32), target.to(torch.float32)))      
-            pred = torch.round(output)
-            
-            correct += pred.eq(target.view_as(pred)).sum().item() 
-    test_loss /= len(test_loader.dataset)                         
-    test_accuracy = correct / len(test_loader.dataset)     
-    return test_loss, test_accuracy
-def prediction(model, data_loader,DEVICE):
+def prediction( data_loader,DEVICE):
+    model = CNN().to(DEVICE)
+    PATH = "/home/cse_urp_dl2/Documents/hhj/BP/saved_models/CNN00.pt"
+    model.load_state_dict(torch.load(PATH))
     model.eval()
     predlist=torch.zeros(0,dtype=torch.float32, device=DEVICE)
     lbllist=torch.zeros(0,dtype=torch.float32, device=DEVICE)
@@ -59,7 +42,8 @@ def prediction(model, data_loader,DEVICE):
 
             predlist=torch.cat([predlist,pred.view(-1)])
             lbllist=torch.cat([lbllist,label.view(-1)])
-
+        
+    # Classification Report
     print(classification_report(lbllist.cpu().numpy(), predlist.cpu().numpy())) 
     print("Precision  :\t"+str(precision_score(lbllist.cpu().numpy(), predlist.cpu().numpy())))
     print("Recall     :\t"+str(recall_score(lbllist.cpu().numpy(), predlist.cpu().numpy() )))
@@ -68,18 +52,15 @@ def prediction(model, data_loader,DEVICE):
     auc_precision_recall = auc(recall, precision)
     print("AUPRC     :\t" + str(auc_precision_recall))
     print("AUROC     :\t" + str(roc_auc_score(lbllist.cpu().numpy(), predlist.cpu().numpy())))
-    return   
-    
-def main():        
-  DEVICE = torch.device('cuda:1') if torch.cuda.is_available() else torch.device('cpu')
-  model_path  = "/home/cse_urp_dl2/Documents/hhj/BP/saved_models/MLP_BN00.pt"
+    return
+ 
+def main():  
+  DEVICE = torch.device('cuda:1') if torch.cuda.is_available() else torch.device('cpu')   
   data_path = "/home/cse_urp_dl2/Documents/hhj/BP/data/"
-  
-  model = MLP_BN().to(DEVICE)
-  model.load_state_dict(torch.load(model_path))
-  
   test_loader = load_test_data(data_path)
-  prediction(model, test_loader,DEVICE)
   
+  
+  prediction( test_loader,DEVICE)
+
 if __name__=="__main__":
     main()
